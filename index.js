8ea5ab7e83b89248019780ad356cf61368c6af93
Dominique Loulie <dom.loulie@gmail.com>
2023-05-27 11:50:49 +1000

    this is my first

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+
+// Write your algorithm here
+
+  const arrayOfLetters = word.split('');
+  const reverseLetters = arrayOfLetters.reverse()
+  const reverseString  = reverseLetters.join('')
+
+  if (word == reverseString){
+
+    return true
+
+  } else {
+    return false
+  }
+
 }
 
-/* 
-  Add your pseudocode here
+  /* 
+  Add your pseudocode here 
+  => The function isPalindrome takes a string as its parameter and evaluates whether it is a palindrome or not. 
+  If the given string matches this definition then the function should return true; if not then false should be returned instead. 
 */
 
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution here 
+  => This function takes a word as an input and splits each letter into an array, 
+  then reverses the order of that array. The reversed array is joined again to form a new word, which is compared against the original word. 
+  If it matches, then true is returned; otherwise false will be returned indicating that the two words are not palindromes.
 */
 
 // You can run `node index.js` to view these console logs
+
 if (require.main === module) {
+
   // add your own custom tests in here
+
+  console.log('Expecting: false')
+  console.log('=>', isPalindrome('boy'));
+
+    //✓ returns false for 'ab'
+    
+  console.log('Expecting: true')
+  console.log('=>', isPalindrome('abba'));
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
